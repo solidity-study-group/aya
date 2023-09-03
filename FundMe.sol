94a08a5e7ef83b2f9908a29d36b49089991c1235
unknown <ayakhedda98@gmail.com>
2023-09-03 19:41:26 +0100

    feat:added_updated_FundMe_and_FallbackExample

--- FundMe.sol
+++ FundMe.sol
@@ -1,33 +1,42 @@
+//Get funds from users
+//Withdraw funds
+//Set a minimum funding value in USD
+
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     //attach PriceConverter functions to uint256s
     using PriceConverter for uint256;
 
     //USD price needs to be $1e18
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded)
         public addressToAmountFunded;
 
-    //syntactic sugar way of writting mappings
-    //mapping (address => uint256) public addressToAmountFunded; for short!
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= minimumUsd,
+            msg.value.getConversionRate() >= MINIMUM_USD,
             "didn't send enough ETH"
         );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for (
             uint256 funderIndex = 0;
             funderIndex < funders.length;
@@ -36,7 +45,34 @@ contract FundMe {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+
         //Reset the funders array after withdrawing everything
         funders = new address[](0);
+
+        (bool callSuccessful, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccessful, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        //require(msg.sender == i_owner, "Sender is not owner!");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    //what if a someone uses a function that doesn't exist
+    //even if someone sends us ETH accidentally instead of calling fundMe(), we should be able to process the tx
+
+    //receive:
+    receive() external payable {
+        fund();
+    }
+
+    //fallback
+    fallback() external payable {
+        fund();
     }
 }
